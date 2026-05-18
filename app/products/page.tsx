46f4a7ccae5c99b5ed5be59c6d3d1d6f4ef605b2
Talvorien <talvorien@gmail.com>
2026-05-19 00:01:25 +0300

    Add full products gallery

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,23 +1,143 @@
-const categories = [
+const productSections = [
   {
     title: "Tops",
-    description: "Blouses, shirts, tops,dresses and printed upperwear pieces.",
-    image: "/products/tops.jpg",
+    description: "Blouses, shirts, jackets, dresses and printed upperwear pieces.",
+    products: [
+      {
+        name: "Black Back Lace-Up Dress",
+        images: [
+          "/products/tops/tops-black-backlaceup-dress-1.png",
+          "/products/tops/tops-black-backlaceup-dress-2.png",
+          "/products/tops/tops-black-backlaceup-dress-3.png",
+        ],
+      },
+      {
+        name: "Black Basic T-Shirt",
+        images: [
+          "/products/tops/tops-black-basic-tshirt-1.png",
+          "/products/tops/tops-black-basic-tshirt-3.png",
+          "/products/tops/tops-black-basic-tshirt-4.png",
+        ],
+      },
+      {
+        name: "Black Beat Move Printed Blouse",
+        images: [
+          "/products/tops/tops-black-beatmoveprinted-blouse-1.png",
+          "/products/tops/tops-black-beatmoveprinted-blouse-2.png",
+          "/products/tops/tops-black-beatmoveprinted-blouse-3.png",
+        ],
+      },
+      {
+        name: "Black White Blouse",
+        images: [
+          "/products/tops/tops-blackwhite-blouse-1.png",
+          "/products/tops/tops-blackwhite-blouse-2.png",
+          "/products/tops/tops-blackwhite-blouse-3.png",
+          "/products/tops/tops-blackwhite-blouse-4.png",
+        ],
+      },
+      {
+        name: "Blue Jean Jacket",
+        images: [
+          "/products/tops/tops-blue-jean-jacket-1.png",
+          "/products/tops/tops-blue-jean-jacket-2.png",
+        ],
+      },
+      {
+        name: "Dark Blue Dress",
+        images: [
+          "/products/tops/tops-darkblue-dress-1.png",
+          "/products/tops/tops-darkblue-dress-2.png",
+          "/products/tops/tops-darkblue-dress-3.png",
+        ],
+      },
+      {
+        name: "Khaki Dress",
+        images: [
+          "/products/tops/tops-khaki-dress-1.png",
+          "/products/tops/tops-khaki-dress-2.png",
+          "/products/tops/tops-khaki-dress-3.png",
+          "/products/tops/tops-khaki-dress-4.png",
+        ],
+      },
+      {
+        name: "Light Blue Shirt",
+        images: [
+          "/products/tops/tops-lightblue-shirt-1.png",
+          "/products/tops/tops-lightblue-shirt-2.png",
+        ],
+      },
+      {
+        name: "Red Blouse",
+        images: [
+          "/products/tops/tops-red-blouse-1.png",
+          "/products/tops/tops-red-blouse-3.png",
+          "/products/tops/tops-red-blouse-4.png",
+        ],
+      },
+      {
+        name: "White Printed Blouse",
+        images: [
+          "/products/tops/tops-white-printed-blouse-1.png",
+          "/products/tops/tops-white-printed-blouse-2.png",
+          "/products/tops/tops-white-printed-blouse-3.png",
+        ],
+      },
+    ],
   },
   {
     title: "Bottoms",
     description: "Trousers, skirts and complementary bottomwear pieces.",
-    image: "/products/bottoms.jpg",
+    products: [
+      {
+        name: "Black Elastic Waist Trousers",
+        images: [
+          "/products/bottoms/bottoms-black-elasticwaist-trousers-1.png",
+          "/products/bottoms/bottoms-black-elasticwaist-trousers-2.png",
+          "/products/bottoms/bottoms-black-elasticwaist-trousers-3.png",
+        ],
+      },
+      {
+        name: "Blue Jean Skirt",
+        images: [
+          "/products/bottoms/bottoms-blue-jean-skirt-1.png",
+          "/products/bottoms/bottoms-blue-jean-skirt-2.png",
+        ],
+      },
+      {
+        name: "Blue Green Jeans",
+        images: [
+          "/products/bottoms/bottoms-bluegreen-jeans-1.png",
+          "/products/bottoms/bottoms-bluegreen-jeans-2.png",
+        ],
+      },
+    ],
   },
   {
     title: "Accessories",
     description: "Selected lifestyle accessories designed to complete the look.",
-    image: "/products/accessories.jpg",
+    products: [],
   },
   {
     title: "Bags",
     description: "Carefully crafted canvas bags and textile-based carry goods.",
-    image: "/products/bags.jpg",
+    products: [
+      {
+        name: "Black Canvas Tote",
+        images: [
+          "/products/bags/bags-canvas-tote-black-middle-1.png",
+          "/products/bags/bags-canvas-tote-black-middle-2.png",
+          "/products/bags/bags-canvas-tote-black-middle-3.png",
+        ],
+      },
+      {
+        name: "White Canvas Tote",
+        images: [
+          "/products/bags/bags-canvas-tote-white-middle-1.png",
+          "/products/bags/bags-canvas-tote-white-middle-2.png",
+        ],
+      },
+    ],
   },
 ];
 
@@ -46,21 +166,44 @@ export default function ProductsPage() {
         </p>
       </section>
 
-      <section className="categoryGrid">
-        {categories.map((category) => (
-          <article className="categoryCard" key={category.title}>
-            <div
-              className="categoryImage"
-              style={{
-                backgroundImage: `url('${category.image}')`,
-              }}
-            ></div>
-
-            <div className="categoryContent">
-              <h2>{category.title}</h2>
-              <p>{category.description}</p>
+      <section className="productSections">
+        {productSections.map((section) => (
+          <div className="productSection" key={section.title}>
+            <div className="productSectionHeader">
+              <h2>{section.title}</h2>
+              <p>{section.description}</p>
             </div>
-          </article>
+
+            {section.products.length > 0 ? (
+              <div className="productGrid">
+                {section.products.map((product) => (
+                  <article className="productCard" key={product.name}>
+                    <div className="productMainImage">
+                      <img src={product.images[0]} alt={product.name} />
+                    </div>
+
+                    <div className="productInfo">
+                      <h3>{product.name}</h3>
+                    </div>
+
+                    <div className="productGallery">
+                      {product.images.map((image, index) => (
+                        <img
+                          src={image}
+                          alt={`${product.name} view ${index + 1}`}
+                          key={image}
+                        />
+                      ))}
+                    </div>
+                  </article>
+                ))}
+              </div>
+            ) : (
+              <div className="comingSoonCard">
+                <p>Coming Soon</p>
+              </div>
+            )}
+          </div>
         ))}
       </section>
     </main>
